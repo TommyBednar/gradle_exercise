297973f2361efb1e102dc98cd41d03971e1efcc1
Tommy <tab114@pitt.edu>
2016-09-27 14:16:19 -0400

    Finish exercise

---
+++ src/main/java/Sequence.java
@@ -0,0 +1,35 @@
+public class Sequence {
+
+    public static int tri(int n) {
+        return n*(n+1)/2;
+    }
+
+    public static int lazy(int n) {
+        return (n*n + n + 2)/2;
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("java Sequence <lazy|triangle> <int32>");
+            System.exit(-1);
+        }
+
+        String seq = args[0];
+
+        if (!seq.equals("lazy") && !seq.equals("tri")) {
+            System.exit(1);
+        }
+        
+        try {
+            int n = Integer.parseInt(args[1]);
+            if (seq.equals("lazy")) {
+               System.out.printf("Lazy(%d) = %d%n", n, lazy(n));
+            } else {
+               System.out.printf("Tri(%d) = %d%n", n, tri(n));
+            }
+        } catch (Exception e) {
+            System.exit(2);
+        }
+
+    }
+}
